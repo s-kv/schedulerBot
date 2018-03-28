a5f30debce6bd0b3f7c5cab270234c85bf458b19
konstantin <mphti@yandex.ru>
2018-03-28 20:33:30 +0300

    Небольшие улучшения интерфейса

--- src/main/java/com/skv/schedulerBot/controller/SchedulerController.java
+++ src/main/java/com/skv/schedulerBot/controller/SchedulerController.java
@@ -12,6 +12,7 @@ import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -29,18 +30,13 @@ public class SchedulerController {
     ScheduleRepository scheduleRepository;
 
     @BotRequestMapping(value = "/get")
-    public SendMessage getWorkerList(Update update) {
-        long chatId = update.getMessage().getChatId();
+    public SendMessage get(Update update) {
+        return getWorkerListMessage(update.getMessage().getChatId());
+    }
 
-        if (workerRepository.findByChatId(chatId) != null)
-            return new SendMessage()
-                    .setChatId(chatId)
-                    .setText("Выберите пользователя:")
-                    .setReplyMarkup(getWorkerKeyboard("get", w -> true));
-        else
-            return new SendMessage()
-                    .setChatId(chatId)
-                    .setText("Вы не зарегстрированы. В доступе отказано");
+    @BotRequestMapping(value = "workerList", messageType = BotRequestMethod.COMMAND)
+    public SendMessage getWorkerListByButton(Update update) {
+        return getWorkerListMessage(update.getCallbackQuery().getMessage().getChatId());
     }
 
     @BotRequestMapping(value = "get", messageType = BotRequestMethod.COMMAND)
@@ -59,7 +55,8 @@ public class SchedulerController {
 
         return new SendMessage()
                 .setChatId(update.getCallbackQuery().getMessage().getChatId())
-                .setText(message.toString());
+                .setText(message.toString())
+                .setReplyMarkup(getScheduleKeyboard("Назад к выбору пользователя"));
     }
 
     @BotRequestMapping(value = "/reg")
@@ -73,22 +70,29 @@ public class SchedulerController {
         else
             return new SendMessage()
                     .setChatId(update.getMessage().getChatId())
-                    .setText("Выберите пользователя:")
+                    .setText("Кто вы?")
                     .setReplyMarkup(getWorkerKeyboard("reg", w -> w.getChatId() == 0));
     }
 
     @BotRequestMapping(value = "reg", messageType = BotRequestMethod.COMMAND)
     public SendMessage regWorker(Update update) {
         long chatId = update.getCallbackQuery().getMessage().getChatId();
-        long id = Long.parseLong(update.getCallbackQuery().getData().split("/")[1].trim());
+        if (workerRepository.findByChatId(chatId) != null)
+            return new SendMessage()
+                    .setChatId(chatId)
+                    .setText("Вы уже зарегистрированы!");
+        else {
+            long id = Long.parseLong(update.getCallbackQuery().getData().split("/")[1].trim());
 
-        Worker worker = workerRepository.findById(id).get();
-        worker.setChatId(chatId);
-        workerRepository.save(worker);
+            Worker worker = workerRepository.findById(id).get();
+            worker.setChatId(chatId);
+            workerRepository.save(worker);
 
-        return new SendMessage()
-                .setChatId(chatId)
-                .setText("Добро пожаловать, " + worker.getFullName() + "!");
+            return new SendMessage()
+                    .setChatId(chatId)
+                    .setText("Добро пожаловать, " + worker.getFullName() + "!")
+                    .setReplyMarkup(getScheduleKeyboard("Получить расписание"));
+        }
     }
 
     @BotRequestMapping(value = "/unreg")
@@ -106,7 +110,7 @@ public class SchedulerController {
         } else {
             return new SendMessage()
                     .setChatId(chatId)
-                    .setText("Вы не зарегстрированы. В доступе отказано");
+                    .setText("Вы не зарегистрированы. В доступе отказано");
         }
     }
 
@@ -115,8 +119,37 @@ public class SchedulerController {
         return new SendMessage()
                 .setChatId(update.getMessage().getChatId())
                 .setText("Команда не поддерживается." + "\n" +
-                        "Используйте команду /get для получения расписания пользователей." + "\n" +
-                        "Для регистрации используйте команду /***");
+                        "Для регистрации используйте команду /***" + "\n" +
+                        "Используйте команду /get для получения расписания пользователей" + "\n" +
+                        "или воспользуйтесь кнопкой ниже")
+                .setReplyMarkup(getScheduleKeyboard("Получить расписание"));
+    }
+
+    private SendMessage getWorkerListMessage(long chatId) {
+        if (workerRepository.findByChatId(chatId) != null)
+            return new SendMessage()
+                    .setChatId(chatId)
+                    .setText("Выберите сотрудника:")
+                    .setReplyMarkup(getWorkerKeyboard("get", w -> true));
+        else
+            return new SendMessage()
+                    .setChatId(chatId)
+                    .setText("Вы не зарегистрированы. В доступе отказано");
+    }
+
+    private InlineKeyboardMarkup getScheduleKeyboard(String callbackText) {
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+
+        List<InlineKeyboardButton> buttonRow = new ArrayList<>();
+        buttonRow.add(new InlineKeyboardButton().setText(callbackText)
+                .setCallbackData("workerList"));
+
+        buttons.add(buttonRow);
+
+        InlineKeyboardMarkup markupKeyboard = new InlineKeyboardMarkup();
+        markupKeyboard.setKeyboard(buttons);
+
+        return markupKeyboard;
     }
 
     private InlineKeyboardMarkup getWorkerKeyboard(String callbackPrefix, Predicate<Worker> check) {
